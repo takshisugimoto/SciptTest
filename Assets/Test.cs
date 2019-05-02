48f28e58db4c2fea63fb30c07e391d2112c5e6fa
takashisugimoto <takashi.kamadoro@icloud.com>
2019-05-02 17:12:09 +0900

    ScriptTest課題コミット
    for文を使い配列の各要素の順番を、順、逆に表示したものです。

---
+++ Assets/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	void Start(){
+		//要素が５個のarrayを初期化する
+		int[] array = { 10, 20, 30, 40, 50 };
+
+		//配列の各要素に順番に繰り返す
+
+
+		for (int i = 0; i < 5; i++) {
+				
+
+			//コンソールに表示させる
+			Debug.Log (array [i]);
+
+		}
+
+		//配列の各要素を逆順に繰り返す
+		for (int i = 4; i >= 0; i--) {
+
+			//コンソールに表示させる
+			Debug.Log (array [i]);
+		}
+	}
+		
+
+			// Update is called once per frame
+	void Update () {
+	}		
+}
